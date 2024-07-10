3d395c588817034a13e3be9d37c65b9a5aa6da6d
sultanaNargis03 <nargissultana969@gmail.com>
2024-07-10 21:15:19 +0530

    "mask"

--- LogicBuilding/Logical/src/BitManipulation/LaunchBitwise.java
+++ LogicBuilding/Logical/src/BitManipulation/LaunchBitwise.java
@@ -2,10 +2,20 @@ package BitManipulation;
 
 public class LaunchBitwise {
 
+	public static void rightShift(int n)
+	{
+		int i=3;
+		int onMask=1<<i;
+		System.out.println(n|onMask);
+		
+	}
 	public static void main(String[] args) {
 	
-		int n=-5;
-		System.out.println(n>>1);
+//		int n=-5;
+//		System.out.println(n>>1);
+		
+		int n=36; //bin=>00100100
+		rightShift(n);
 
 	}
 
