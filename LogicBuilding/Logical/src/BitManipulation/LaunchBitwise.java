ffc5b4f5ce8183c3a54c2e3fed7b075dc77fd47a
sultanaNargis03 <nargissultana969@gmail.com>
2024-07-10 21:56:28 +0530

    "switch on and off--masking"

--- LogicBuilding/Logical/src/BitManipulation/LaunchBitwise.java
+++ LogicBuilding/Logical/src/BitManipulation/LaunchBitwise.java
@@ -2,12 +2,30 @@ package BitManipulation;
 
 public class LaunchBitwise {
 
-	public static void rightShift(int n)
+	public static int switchOn(int n)
 	{
 		int i=3;
 		int onMask=1<<i;
-		System.out.println(n|onMask);
+		return (n|onMask);
 		
+	}
+	public static int switchOff(int n)
+	{
+		int i=5;
+		int offMask=~(1<<i);
+		return (n&offMask);
+		
+	}
+	public static String d2b(int  n)
+	{
+		String res="";
+		while(n>0)
+		{
+			int rem=n%2;
+			res=rem+res;
+			n=n/2;
+		}
+		return res;
 	}
 	public static void main(String[] args) {
 	
@@ -15,7 +33,16 @@ public class LaunchBitwise {
 //		System.out.println(n>>1);
 		
 		int n=36; //bin=>00100100
-		rightShift(n);
+		
+		System.out.println(d2b(n));
+		
+		int on=switchOn(n);
+		int off=switchOff(n);
+		
+		String binOn=d2b(on);
+		String binOff=d2b(off);
+		System.out.println("switch on ith bit "+on+"->"+ binOn);
+		System.out.println("switch off ith bit "+off+"->"+binOff );
 
 	}
 
